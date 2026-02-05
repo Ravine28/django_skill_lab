bad68d7c46d04cd9c2972199f5c0b3a8bcf60ccc
Ravine28 <ravineaparecida@gmail.com>
2026-02-05 11:35:09 -0300

    build(project): ajustes em arquivos/diretórios

--- django_skill_labs/app_django_lab/views.py
+++ django_skill_labs/app_django_lab/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def clientes(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'app_django_lab/django_lab.html')
+
