db0575714c0e65e9fef4504cb7d5bf1108d5e90f
Ravine28 <ravineaparecida@gmail.com>
2026-02-06 03:36:16 -0300

    refactor: replace app_cliente and app_sistema with app_acesso

---
+++ django_skill_labs/app_acesso/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppAcessoConfig(AppConfig):
+    name = 'app_acesso'
--- django_skill_labs/app_sistema/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppSistemaConfig(AppConfig):
-    name = 'app_sistema'
