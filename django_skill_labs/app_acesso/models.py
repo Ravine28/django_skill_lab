021605e83e8fabbb6e822235aa3fe347c5b6758f
Ravine28 <ravineaparecida@gmail.com>
2026-02-06 03:58:48 -0300

    feat/models: create rules Cliente and Sistema

--- django_skill_labs/app_acesso/models.py
+++ django_skill_labs/app_acesso/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Sistema(models.Model):
+    nome = models.CharField(max_length=100) # Classe que define o nome do sistema, sendo este do tipo caracter 
+
+    def __str__(self):
+        return self.nome # Função que retorna o nome do Sistema inserido pelo usuário
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=100) # Classe que define o nome do cliente, sendo este do tipo caracter
+
+    acesso = models.ManyToManyField(Sistema, blank=True) # ManyToManyField para criar a relação entre Cliente e Sistema. O comando blank=True permite que um cliente possa ser criado sem acesso a nenhum sistema inicialmente.
+    def __str__(self):
+        return self.nome # Função que retorna o nome do Cliente inserido pelo usuário
