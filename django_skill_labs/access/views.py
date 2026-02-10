f33abed856e5cfc94257ef092a2d90a60b5a9ba5
Ravine28 <ravineaparecida@gmail.com>
2026-02-10 14:56:15 -0300

    refactor/project: ajustes em estrutura geral do projeto

--- django_skill_labs/access/views.py
+++ django_skill_labs/access/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import get_object_or_404
 from .models import Access, Client, System
 
 def access(request):
-  access_list_html = Access.objects.all().values()
+  access_list_html = Access.objects.all()
   template = loader.get_template('access_all.html')
   context = {
     'access_list_html': access_list_html,
@@ -11,7 +12,7 @@ def access(request):
   return HttpResponse(template.render(context, request))
 
 def access_details(request, id):
-  access_details_list_html = Access.objects.get(id=id)
+  access_details_list_html = get_object_or_404(Access, id=id)
   template = loader.get_template('access_details.html')
   context = {
     'access_details_list_html': access_details_list_html,
@@ -19,7 +20,7 @@ def access_details(request, id):
   return HttpResponse(template.render(context, request))
 
 def system_access(request):
-  system_access_list_html = System.objects.all().values()
+  system_access_list_html = System.objects.all()
   template = loader.get_template('system_access.html')
   context = {
     'system_access_list_html': system_access_list_html,
@@ -27,7 +28,7 @@ def system_access(request):
   return HttpResponse(template.render(context, request))
 
 def client_access(request):
-  client_access_list_html = Client.objects.all().values()
+  client_access_list_html = Client.objects.all()
   template = loader.get_template('client_access.html')
   context = {
     'client_access_list_html': client_access_list_html,
