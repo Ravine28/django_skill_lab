b394b22d3d106c6440c4eacc1c6061655ef12186
Ravine28 <ravineaparecida@gmail.com>
2026-02-20 00:53:29 -0300

    feat(models): refatoração de modelos e atualização de campos para consistência feat(templates): atualização de templates para refletir mudanças nos modelos build(urls): reorganização das importações no arquivo de URLs

--- django_skill_labs/access/models.py
+++ django_skill_labs/access/models.py
@@ -1,87 +1,27 @@
 from django.db import models
 
 class System(models.Model):
-    system_name = models.CharField(max_length=100)
-    '''
-    Variável de identificação do Sistema, sendo este do tipo AutoField
-    que define o nome do Sistema, sendo este do tipo caracter (curto)
-    '''
-
-    system_description  = models.TextField()
-    ''' 
-    Variável de identificação do Sistema, sendo este do tipo AutoField
-    de texto para a descrição do Sistema. O TextField é usado para armazenar 
-    grandes quantidades de texto.
-    '''
-
-    system_creation_date = models.DateTimeField(auto_now_add=True)
-    '''
-    Variável de identificação do Sistema, sendo este do tipo AutoField
-    de data e hora para registrar o momento da criação do Sistema, com a opção de 
-    adicionar automaticamente a data e hora atual
-    '''
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.system_name
-    '''
-    Função que retorna o nome do Sistema inserido pelo usuário
-    '''
+        return self.name
 
 class Client(models.Model):
-    client_name = models.CharField(max_length=100)
-    '''
-    Variável que recebe o nome do cliente, sendo este do tipo caracter (curto)
-    '''
-    
-    client_cpf_cnpj = models.CharField(max_length=20)
-    ''' 
-    Variável de texto para o CPF ou CNPJ do cliente. O CharField é usado para armazenar
-    pequenos textos, como números de identificação.
-    '''
-
-    client_email = models.EmailField()
-    '''
-    Variável que recebe o email de contato do cliente
-    '''
-
-    client_creation_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    cpf_cnpj = models.CharField(max_length=20)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.client_name
-    '''
-    Função que retorna o nome do Cliente inserido pelo usuário
-    '''
+        return self.name
 
 class Access(models.Model):
-    access_client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    '''
-    Variável de estrangeira para a função Cliente, com a opção de exclusão em cascata
-    '''
-
-    access_system = models.ForeignKey(System, on_delete=models.CASCADE)
-    '''
-    Variável de chave estrangeira para a função Sistema, com a opção de exclusão em cascata
-    '''
-    
-    access_creation_date = models.DateTimeField(auto_now_add=True)
-    '''
-    Variável que recebe data e hora para registrar o momento da criação do Acesso, com a opção de 
-    adicionar automaticamente a data e hora atual
-    '''
-
-    access_date = models.DateTimeField(auto_now_add=True)
-    '''
-    Variável que recebe data e hora para registrar o momento do acesso, com a opção de adicionar 
-    automaticamente a data e hora atual
-    '''
-
-    is_active = models.BooleanField(default=True)
-    '''
-    Verificar se o acesso está ativo ou não
-    '''
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name="accesses")
+    system = models.ForeignKey(System, on_delete=models.CASCADE, related_name="accesses")
+    created_at = models.DateTimeField(auto_now_add=True) 
+    is_active = models.BooleanField(default=True) #campo para REVOKE
 
     def __str__(self):
-        return f"Acesso de {self.access_client.client_name} ao sistema {self.access_system.system_name}"
-    '''
-    Função que retorna uma string formatada com o nome do cliente e do sistema em que o mesmo está acessando
-    '''
+        return f"Acesso: {self.client.name} ao {self.system.name}"
